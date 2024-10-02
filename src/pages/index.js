d7643d29b0639ffb303a2aa2990d201b6d6adb1a
Laurent Schwartz <contact@zikapanam.fr>
2024-10-02 08:23:15 +0200

    update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Toutes les informations sur l'association
+            ➡️ Toutes les informations sur l'association
           </Link>
         </div>
       </div>
